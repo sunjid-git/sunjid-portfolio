eb9449660346d5c7f085bbfe5b3ae54ac73989d1
sunjid-git <sunjid.info@gmail.com>
2021-05-06 13:15:32 +0600

    blog updating

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import gifOne from '../../images/gifOne.gif';
-import gifTwo from '../../images/gifTwo.gif';
-import gifThree from '../../images/gifThree.gif';
+import b1 from '../../images/b1.jpeg';
+import b2 from '../../images/b2.jpg';
+import b3 from '../../images/b3.jpg';
 
+import './Blog.css';
 const Blog = () => {
     return (
         <div className="background">
@@ -13,25 +14,43 @@ const Blog = () => {
             <div className="row">
                     
                 <div className="col-md-4 d-flex justify-content-center">
-                    <div className="projectCard mb-5">
-                        <img className="serviceImg" src={gifOne} alt=""/>
-                        <h5 className="text-center text-success p-3">Coming Soon</h5>
+                <a className="nav-link active  text-light" href="https://sunjid-web.medium.com/network-topology-686c7de9f973" target="blank">
+                    <div className="blogCard mb-5">
+                        <img className="serviceImg" src={b1} alt=""/>
+                        <div className="text-center text-light m-3">
+                        <h5 className="py-2">10 Most commonly used Javascript string methods</h5>
+                        <p className="text-">This article is all about some basic methods that we all use oftenly.</p>
+                        </div>
                     </div>
+                    </a>
                 </div>
 
                 <div className="col-md-4 d-flex justify-content-center">
-                    <div className="projectCard mb-5">
-                        <img className="serviceImg" src={gifTwo} alt=""/>
-                        <h5 className="text-center text-success p-3">Coming Soon</h5>
+                <a className="nav-link active  text-light" href="https://sunjid-web.medium.com/network-topology-686c7de9f973" target="blank">
+                    <div className="blogCard mb-5">
+                        <img className="serviceImg" src={b2} alt=""/>
+                        <div className="text-center text-light m-3">
+                        <h5 className="py-2">Network Topology</h5>
+                        <p className="text-"> Basic Network Topology with visualization are discussed here.</p>
+                        </div>
                     </div>
+                    </a>
                 </div>
 
                 <div className="col-md-4 d-flex justify-content-center">
-                    <div className="projectCard mb-5">
-                        <img className="serviceImg" src={gifThree} alt=""/>
-                        <h5 className="text-center text-success p-3">Coming Soon</h5>
+
+                <a className="nav-link active  text-light" href="https://sunjid-web.medium.com/10-most-commonly-used-javascript-string-methods-eac3dfb5e914" target="blank">
+                    <div className="blogCard mb-5">
+                        <img className="serviceImg" src={b3} alt=""/>
+                        <div className="text-center text-light m-3">
+                        <h5 className="py-2">Network protocols and standards organizations</h5>
+                        <p className="text-">What is network protocols and which companies are behind the the network world are introduced here. </p>
+                        </div>
                     </div>
+                    </a>
+
                 </div>
+
             </div>
         </div>
         </div>
